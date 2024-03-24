2e8baf96f56e2e9373391d8a7e0e585b422c0f1c
Kristinaliebhart <56191285+Kristinaliebhart@users.noreply.github.com>
2024-03-24 13:38:27 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -15,7 +15,7 @@ class Config {
 
 
      //TODO: change for studie:
-    static run = "1";                                   //1-6 or 1-3
+    static run = "2";                                   //1-6 or 1-3
     static input_device = "Stylus"                      //Finger or Stylus
     static isTestSet = true;                           // Determines, if test or trainings set will be used for trials
 
